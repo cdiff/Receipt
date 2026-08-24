23e6c24c4d30b9366d0bb6014a85632a4a26a49a
이태수 <pasic1@naver.com>
2026-08-24 17:52:16 +0900

    chore(release): 구글 플레이 비공개 테스트 업데이트를 위한 versionCode 상향 (v3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.pasic.receipt"
         minSdk = 31
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
