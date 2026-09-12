57be9049024b328a83630cdd70b4974d66e393f0
이태수 <pasic1@naver.com>
2026-09-12 23:43:44 +0900

    chore: Google Play 중복 방지를 위해 versionCode 7로 상향

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.pasic.receipt"
         minSdk = 31
         targetSdk = 36
-        versionCode = 6
+        versionCode = 7
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
